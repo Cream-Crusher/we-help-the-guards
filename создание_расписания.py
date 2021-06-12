466dc7a87efbef1c09a829ab178efc53bc173fe8
Никита <64539102+ruk228@users.noreply.github.com>
2021-06-12 22:24:15 +0400

    Creating a schedule
    1) Uploaded the code

--- создание_расписания.py
+++ создание_расписания.py
@@ -1,25 +1,19 @@
-import docx
 import os
-from random import randint
+from random import randint, randrange
 from datetime import timedelta, datetime
+from docxtpl import DocxTemplate
 
 
-def ворлд(now, schedule_territories):
+def world(now, schedule_territories):
     now_day = datetime(now.year, now.month, now.day)
-    name_file = 'расписание_охраны.docx'
-    document = docx.Document()
-    document.save(name_file)
-    doc = docx.Document(name_file)
-    doc.add_heading('Докладная записка', 1)
-    doc.add_heading('Докладываю Вам, что с {} по {} обходы территории склада осуществлялись:'.format(now_day,now_day + timedelta(days=1)), 3)
-    
-    for territory in schedule_territories:
-        doc.add_heading('{}__________:'.format(territory), 3)
-
-        for schedule in schedule_territories[territory]:
-            doc1 = doc.add_heading('Убыл:{}'.format(schedule), 4)
-            doc1.add_run('   Прибыл:____________')
-    doc.save(name_file)
+    doc = DocxTemplate("pattern.docx")
+    context = {
+    'now_day_from': now_day,
+    'now_day_before': now_day + timedelta(days=1),
+    'schedule_territories': schedule_territories
+    }
+    doc.render(context)
+    doc.save("security_guard_bypass_schedule.docx")
 
 
 def get_a_schedule_rounded_to_5(set_time, base=5):
@@ -29,14 +23,23 @@ def get_a_schedule_rounded_to_5(set_time, base=5):
     for territory in territories:
         schedule = []
         crawl_time = set_time
+        num = 0
 
         while crawl_time.hour != 7 and crawl_time.hour != 6:
+            
+            if num == 0:
+                crawl_time = set_time - timedelta(minutes=randrange(-5, 15, 5))
+                schedule.append(crawl_time)
+
+            num = 1
             time_between_crawl = randint(90, 120)
             time_between_crawl = base * round(time_between_crawl/base)
             after_the_crawl = timedelta(minutes=time_between_crawl)
             crawl_time = crawl_time+after_the_crawl
             schedule.append(crawl_time)
+
         schedule_territories.update({territory: schedule})
+
     return schedule_territories
 
 
@@ -49,5 +52,4 @@ def get_date_and_time():
 if __name__ == '__main__':
     now, set_time = get_date_and_time()
     schedule_territories = get_a_schedule_rounded_to_5(set_time)
-    ворлд(now, schedule_territories)
-    
+    world(now, schedule_territories)
